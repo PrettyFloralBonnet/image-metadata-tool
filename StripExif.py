6cf6020985b3c54c1d190a47c87fbb58ca858748
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2022-12-25 19:45:53 +0100

    refactor into OOP style

--- StripExif.py
+++ StripExif.py
@@ -1,84 +1,100 @@
 import os
 import sys
-import glob
 import exifread
 
 from PIL import Image
+from typing import Dict
 
 
-def strip_metadata(path):
-    file_name = os.path.splitext(path)[0]  # TODO: store this in an instance attr
+class MetadataImage:
+    def __init__(self, path) -> None:
+        self.path = path
+        self._metadata = None
 
-    try:
-        original_image = Image.open(path)
-    except IOError:
-        print(f"The file {os.path.basename(path)} is not a valid image.")
-    
-    og_image_data = original_image.getdata()
+    @property
+    def name(self) -> str:
+        """Returns the name of the image file (without extension)."""
 
-    new_image = Image.new(original_image.mode, original_image.size)
-    new_image.putdata(og_image_data)
+        return os.path.splitext(self.path)[0]
 
-    output_dir = os.path.join(os.getcwd(), "output")
+    @property
+    def metadata(self) -> Dict:
+        """Returns all image metadata."""
 
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+        if self._metadata is None:
+            with open(self.path, "rb") as im:
+                # file type validation is handled internally by exifread
+                tags = exifread.process_file(im)
+                self._metadata = tags
 
-    new_image.save(os.path.join(output_dir, f"{file_name}_stripped.JPEG"))
+        return self._metadata
 
-# borrowed from https://gist.github.com/snakeye/fdc372dbf11370fe29eb
-def _convert_to_degrees(value):
-    """Converts the GPS coordinates stored in the EXIF to degrees.
+    @staticmethod
+    def _convert_GPS_to_degrees(value: exifread.classes.IfdTag) -> float:
+        """Converts the GPS coordinates stored in the metadata to degrees.
 
-    :param value: a ratio type value returned by exifread
-    :returns a geographical coordinate (latitude or longitude) as a float
-    """
+        :param value: a ratio type value returned by exifread
+        :returns a geographical coordinate (latitude or longitude) as a float
+        """
 
-    d = float(value.values[0].num) / float(value.values[0].den)
-    m = float(value.values[1].num) / float(value.values[1].den)
-    s = float(value.values[2].num) / float(value.values[2].den)
+        # borrowed from https://gist.github.com/snakeye/fdc372dbf11370fe29eb
 
-    return d + (m / 60.0) + (s / 3600.0)
+        d = float(value.values[0].num) / float(value.values[0].den)
+        m = float(value.values[1].num) / float(value.values[1].den)
+        s = float(value.values[2].num) / float(value.values[2].den)
 
-# based on https://gist.github.com/snakeye/fdc372dbf11370fe29eb
-def getGPS(path):
-    """Returns the GPS coordinates, if present.
-    
-    :param filepath: the path to the file to retrieve the coordinates from
-    """
+        return d + (m / 60.0) + (s / 3600.0)
 
-    with open(path, 'rb') as f:
-        tags = exifread.process_file(f)  # exifread handles image file validation
+    @property
+    def GPS_metadata(self) -> Dict:
+        """Returns GPS related metadata only."""
 
-        latitude = tags.get('GPS GPSLatitude')
-        latitude_ref = tags.get('GPS GPSLatitudeRef')
-        longitude = tags.get('GPS GPSLongitude')
-        longitude_ref = tags.get('GPS GPSLongitudeRef')
+        # based on https://gist.github.com/snakeye/fdc372dbf11370fe29eb
+
+        latitude = self.metadata.get('GPS GPSLatitude')
+        latitude_ref = self.metadata.get('GPS GPSLatitudeRef')
+        longitude = self.metadata.get('GPS GPSLongitude')
+        longitude_ref = self.metadata.get('GPS GPSLongitudeRef')
 
         if latitude is not None:
-            latitude = _convert_to_degrees(latitude)
+            latitude = self._convert_GPS_to_degrees(latitude)
             if latitude_ref.values != 'N':
                 latitude = -latitude
 
         if longitude is not None:
-            longitude = _convert_to_degrees(longitude)
+            longitude = self._convert_GPS_to_degrees(longitude)
             if longitude_ref.values != 'E':
                 longitude = -longitude
 
-    return {'latitude': latitude, 'longitude': longitude}
+        return {'latitude': latitude, 'longitude': longitude}
 
-def process_image(path):
-    file_name = os.path.splitext(path)[0]  # TODO: retrieve it from an instance attr
+    def save_copy_without_metadata(self) -> None:
+        """Saves a copy of the image stripped from all metadata."""
 
-    print(getGPS(path))
-    strip_metadata(path)
-    print(getGPS(os.path.join("output", f"{file_name}_stripped.JPEG")))
+        if len(self.metadata) == 0:
+            print(f"The image {self.name} has no metadata. No copy will be created.")
+            return
+
+        try:
+            image = Image.open(self.path)
+        except IOError:
+            print(f"The file {os.path.basename(self.path)} is not a valid image.")
+
+        # this just gets the data which forms the actual image, without metadata
+        image_data = image.getdata()
+
+        stripped_image = Image.new(image.mode, image.size)
+        stripped_image.putdata(image_data)
+        stripped_image.save(
+            os.path.join(os.getcwd(), f"{self.name}_stripped.JPEG")
+        )
+
 
 if __name__ == "__main__":
-    if os.path.isdir(sys.argv[1]):  # seems to ignore the output directory
-        files = glob.glob(os.path.join(sys.argv[1], "*"))
-    else:
-        files = sys.argv[1:]
-
-    for im in files:
-        process_image(im)
+    image = MetadataImage(sys.argv[1])
+    print(image.name)
+    print("\n")
+    print(image.metadata)
+    print("\n")
+    print(image.GPS_metadata)
+    image.save_copy_without_metadata()
