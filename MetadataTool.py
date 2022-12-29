0ddc3f10dea07487137f30455f207bba6c09eddc
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2022-12-29 21:41:45 +0100

    start working on the MetadataTool class

--- MetadataTool.py
+++ MetadataTool.py
@@ -1,6 +1,28 @@
+import os
 import sys
 
 from MetadataImage import MetadataImage
+
+
+class MetadataTool:
+    def __init__(self, input_path) -> None:
+        self._input_path = input_path
+        self.images_to_process = []
+
+    @property
+    def input(self):
+        if os.path.isdir(self._input_path):
+            pass
+    pass
+
+    def prepare_images(self):
+        if os.path.isfile(self._input_path):
+            self.images_to_process.append(self._input_path)
+
+        if os.path.isdir(self._input_path):
+            # https://stackoverflow.com/questions/71112986/retrieve-a-list-of-supported-read-file-extensions-formats
+            pass
+
 
 if __name__ == "__main__":
     # WIP
